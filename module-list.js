9fc187ca896762c553942e350fced4c3be4cbd6c
Gunnar Unger <gunnar.unger@gmail.com>
2020-06-10 14:43:19 +1000

    update

--- module-list.js
+++ module-list.js
@@ -9,7 +9,7 @@
         "calendar-sleepstudy-scoring-month": { url: "$H/m/calendar-sleepstudy-scoring-month.html", Table: "demo-wapp-booking", Table2: "demo-wapp-scoring" },
         "calendar-sleepstudy-report-month": { url: "$H/m/calendar-sleepstudy-report-month.html", Table: "demo-wapp-booking", Table2: "demo-wapp-report" },
         "calendar-sleepstudy-day": { url: "$H/m/calendar-sleepstudy-day.html", Table: "demo-wapp-booking", Table2: "demo-wapp-assign", Table3: "demo-wapp-setup", Table4: "demo-wapp-scoring", Table5: "demo-wapp-report", "booking": "booking-form", "assign": "assign-form", "setup": "setup-form", "scoring": "scoring-form", "report": "report-form" },
-        "calendar-sleepstudy-assign-day": { url: "$H/m/calendar-sleepstudy-assign-day.html", Table: "demo-wapp-booking", Table2: "demo-wapp-assign", Table3: "demo-wapp-roster", Table4: "demo-wapp-room", "booking": "booking-form", "assign": "assign-form", "setup": "setup-form", "scoring": "scoring-form", "report": "report-form" },
+        "calendar-sleepstudy-assign-day": { url: "$H/m/calendar-sleepstudy-assign-day.html", Table: "demo-wapp-booking", Table2: "demo-wapp-assign", Table3: "demo-wapp-roster", Table4: "demo-wapp-room", "booking": "booking-form", "assign_drop": "assign-drop-form", "assign": "assign-form", "scoring": "scoring-form", "report": "report-form" },
         "booking-form": { url: "$H/m/booking-form.html", Table: "demo-wapp-booking", lookup: "demo-wapp-patient", lookup2: "demo-wapp-diagnostic-procedure", lookup3: "demo-wapp-research-project", lookup4: "demo-wapp-physician" },
         "booking-data": { url: "$H/m/booking-data.html", Table: "demo-wapp-booking", form_module: "booking-form" },
         "setup-form": { url: "$H/m/setup-form.html", Table: "demo-wapp-setup", Table2: "demo-wapp-booking", Table3: "demo-wapp-patient" },
@@ -34,6 +34,7 @@
         "room-form": { url: "$H/m/room-form.html", Table: "demo-wapp-room" },
         "assign-data": { url: "$H/m/assign-data.html", Table: "demo-wapp-assign", form_module: "assign-form" },
         "assign-form": { url: "$H/m/assign-form.html", Table: "demo-wapp-assign", lookup: "demo-wapp-room", lookup2: "demo-wapp-roster" , lookup3: "demo-wapp-patient" },
+        "assign-drop-form": { url: "$H/m/assign-drop-form.html", Table: "demo-wapp-assign", lookup: "demo-wapp-room", Table2: "demo-wapp-roster", Table3: "demo-wapp-room" , lookup3: "demo-wapp-patient" },
     }
     for (p in modules) {
         $vm.module_list[p] = modules[p];
